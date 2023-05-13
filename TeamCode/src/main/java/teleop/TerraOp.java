a81014863b0f90472b64e937b3922c93f0c2e253
Riyansh Goyal <go.riyansh@gmail.com>
2023-05-13 10:13:07 -0700

    tried fixing movement speed

--- TeamCode/src/main/java/teleop/TerraOp.java
+++ TeamCode/src/main/java/teleop/TerraOp.java
@@ -1,33 +1,37 @@
 package teleop;
 
 import static global.General.gph1;
+import static global.General.log;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import global.Modes;
 import teleutil.button.Button;
-import teleutil.button.OnTurnOnEventHandler;
 
 @TeleOp(name = "TerraOp", group = "TeleOp")
 public class TerraOp extends Tele {
 
     @Override
     public void initTele() {
-
+        Modes.driveMode.set(Drive.MEDIUM);
+        gph1.link(Button.DPAD_UP, () -> Modes.driveMode.set(Drive.FAST));
+        gph1.link(Button.DPAD_LEFT, () -> Modes.driveMode.set(Drive.MEDIUM));
+        gph1.link(Button.DPAD_RIGHT, () -> Modes.driveMode.set(Drive.MEDIUM));
+        gph1.link(Button.DPAD_DOWN, () -> Modes.driveMode.set(Drive.SLOW));
     }
 
     @Override
     public void loopTele() {
-        gph1.link(Button.RIGHT_STICK_X, OnTurnOnEventHandler.class, () -> drive.move(0,1,0));
-        gph1.link(Button.RIGHT_STICK_Y, OnTurnOnEventHandler.class, () -> drive.move(1,0,0));
-        gph1.link(Button.LEFT_STICK_X, OnTurnOnEventHandler.class, () -> drive.move(0,0,1));
-
-
-        gph1.link(Button.DPAD_UP, OnTurnOnEventHandler.class, () -> Modes.driveMode.set(Drive.FAST));
-        gph1.link(Button.DPAD_LEFT, OnTurnOnEventHandler.class, () -> Modes.driveMode.set(Drive.MEDIUM));
-        gph1.link(Button.DPAD_RIGHT, OnTurnOnEventHandler.class, () -> Modes.driveMode.set(Drive.MEDIUM));
-        gph1.link(Button.DPAD_DOWN, OnTurnOnEventHandler.class, () -> Modes.driveMode.set(Drive.SLOW));
+        if(driveMode.modeIs(Drive.FAST)){
+            drive.move(gph1.ry, gph1.rx, gph1.lx);
+        } else if (driveMode.modeIs(Drive.SLOW)){
+            drive.move(0.25*gph1.ry, 0.25*gph1.rx, 0.25*gph1.lx);
+        } else if (driveMode.modeIs(Drive.MEDIUM)) {
+            drive.move(0.5*gph1.ry, 0.5*gph1.rx, 0.5*gph1.lx);
+        }
+        log.show(driveMode.get());
     }
+
 
 }
 
