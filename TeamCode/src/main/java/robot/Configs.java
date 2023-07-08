159f10f7ccd4dc75e54749e94ef3335f47bf4088
nithin <nithinlokesh125@gmail.com>
2023-07-08 10:10:35 -0700

    Reverted session 6 changes

--- TeamCode/src/main/java/robot/Configs.java
+++ TeamCode/src/main/java/robot/Configs.java
@@ -20,7 +20,7 @@ public class Configs implements RobotUser{
 
     // TASK 1: Remove the gyro sensor from the config
     RobotConfig CampConfig = new RobotConfig(drive, gyro);
-
+//
 
     /**
      * Current Config
