eac093faed4186d6af8f60aa855e4f0c64185a3c
Nithin <58347462+nithinnnn@users.noreply.github.com>
2023-08-31 20:33:41 -0700

    v4.42.5 - odo is failing, roadrunner time

--- TeamCode/src/main/java/auton/TerraAutoOdometry.java
+++ TeamCode/src/main/java/auton/TerraAutoOdometry.java
@@ -17,26 +17,22 @@ import elements.FieldSide;
 import elements.GameItems;
 import geometry.position.Pose;
 
-//@Autonomous(name = "TerraAutoOdometry" +
-//        "", group = "auto", preselectTeleOp = "TerraOp")
+@Autonomous(name = "TerraAutoOdometry" +
+        "", group = "auto", preselectTeleOp = "TerraOp")
 public class TerraAutoOdometry extends AutoFramework {
 
 
     @Override
     public void initialize() {
         this.setConfig(NonstopConfig);
-        bot.saveLocationOnField();
+
     }
 
     @Override
     public void define() {
         addTimedSetpoint(1.0, 0.8, 10, 0, 115, 0);
-        addSegment(1.0, DefaultSP, 0, 115, 0);
+
     }
-
-    @Autonomous(name = "TerraOdoTest", group = "auto", preselectTeleOp = "TerraOp")
-    public static class TerraOdoTest extends TerraAutoOdometry {{ fieldSide = FieldSide.BLUE; fieldPlacement = FieldPlacement.LOWER; startPose = new Pose(20.5, Field.width/2.0 - Field.tileWidth - GameItems.Cone.height - 16,90); }}
-
 
 }
 
