06152aed4fb561b23a84388c6a64ee8180d929a7
Nithin <58347462+nithinnnn@users.noreply.github.com>
2023-12-06 18:12:59 -0800

    4.43.3 - Edited riseop

--- TeamCode/src/main/java/teleop/TerraRiseOp.java
+++ TeamCode/src/main/java/teleop/TerraRiseOp.java
@@ -2,22 +2,28 @@ package teleop;
 
 import static global.General.gph1;
 import static global.General.log;
+import static teleutil.button.Button.A;
+import static teleutil.button.Button.LEFT_TRIGGER;
+import static teleutil.button.Button.RIGHT_TRIGGER;
+import static teleutil.button.Button.X;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import teleutil.button.Button;
 
+@TeleOp(name = "RiseOp", group = "TeleOp")
 public class TerraRiseOp extends Tele {
     @Override
     public void initTele() {
-        gph1.link(Button.RIGHT_TRIGGER ,claw::closeClaw);
-        gph1.link(Button.LEFT_TRIGGER,claw::openClaw);
-        gph1.link(Button.A,arm::liftArm);
-        gph1.link(Button.X,arm::resetArm);
+//        gph1.link(RIGHT_TRIGGER ,claw::closeClaw);
+//        gph1.link(LEFT_TRIGGER,claw::openClaw);
+//        gph1.link(A,arm::liftArm);
+//        gph1.link(X,arm::resetArm);
 
     }
 
     @Override
     public void loopTele() {
-log.show("heightMode is: " + heightMode);
         drive.move(gph1.ry,gph1.rx,gph1.lx);
     }
 }
