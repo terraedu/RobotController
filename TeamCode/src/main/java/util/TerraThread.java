2585fb19fabd5896be6aaba09f33a329ea8d2580
Nithin <58347462+nithinnnn@users.noreply.github.com>
2024-12-20 20:04:09 -0800

    4.45.6

--- TeamCode/src/main/java/util/TerraThread.java
+++ TeamCode/src/main/java/util/TerraThread.java
@@ -114,7 +114,7 @@ public class TerraThread extends Thread{
             try {
                 updateCode.run();
             } catch (RuntimeException r){
-                r.printStackTrace();
+                log.show(r.getStackTrace());
                 wasExceptionThrown = true;
                 stopUpdating();
             }
